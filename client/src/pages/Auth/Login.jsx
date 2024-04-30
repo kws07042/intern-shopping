1c2c314d6df2baa6c10938691a5c9761bcd3bf4b
japark <wkd5878@gmail.com>
2024-04-30 12:33:47 +0900

    init: set pages, set router

---
+++ client/src/pages/Auth/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Login() {
+    return (
+        <div>
+            <form action="">
+                {/* Test ID(Email) */}
+                <input type="text"/>
+
+                {/* Test Password */}
+                <input type="text"/>
+            </form>
+        </div>
+    );
+}
---
+++ client/src/pages/Auth/Register.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Register() {
+    return (
+        <div>
+            <form action="">
+                {/* Test ID(Email) */}
+                <input type="text"/>
+
+                {/* Test Password */}
+                <input type="text"/>
+            </form>
+        </div>
+    );
+}
