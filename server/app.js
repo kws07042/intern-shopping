ca4f668918e5d134e55d172ff296946e1db51964
JangHwanPark <wkd5878@gmail.com>
2024-05-03 05:48:32 +0900

    feat: cors 설정

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 
 // Import Route modules
@@ -7,7 +8,14 @@ import signUpRouter from './routes/signUp.js';
 import signInRouter from './routes/signIn.js';
 import productsRouter from './routes/products.js';
 
+// Express 객체 생성
 const app = express();
+
+// CORS 설정
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 
 // 미들웨어 설정
 app.use(bodyParser.json()); // JSON 요청 본문을 파싱목적
@@ -19,6 +27,7 @@ app.use('/signup', signUpRouter);
 app.use('/signin', signInRouter);
 app.use('/products', productsRouter);
 
+// 서버 실행
 app.listen(8080, () => {
     console.log('서버 시작')
 });
