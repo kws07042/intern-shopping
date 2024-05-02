30bda0a1627698208a17c978a27c28d55cdcfa6f
JangHwanPark <wkd5878@gmail.com>
2024-05-03 06:39:26 +0900

    컴포넌트 파일 생성

---
+++ client/src/components/CarouselSlider/CarouselSlider.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CarouselSlider() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
---
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SearchBar() {
+    return (
+        <form>
+            <input type="text" placeholder="Search..." />
+            <button>Search</button>
+        </form>
+    );
+}
