153d7e77f27880a98fca3cab19811e91d07ca793
geonyoungKO <kws07042@gmail.com>
2024-05-05 03:18:53 +0900

    Footer.jsx 수정

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {NavLink} from "react-router-dom";
 
 export default function Footer() {
     return (
-        <footer>
-            <nav>
-                {/*React에서는 a 태그 대신에 Link사용*/}
-                <ul className="footer-nav">
-                    <li><Link to="/shipping">배송정보</Link></li>
-                    <li><Link to="/exchange">교환 및 반품</Link></li>
-                    <li><Link to="/terms">이용약관</Link></li>
-                    <li><Link to="/contact">문의하기</Link></li>
-                    <li><Link to="/privacy">개인정보처리방침</Link></li>
-                </ul>
-            </nav>
+        <nav>
+            <ul className={"footer-nav"}>
+                <li>
+                <NavLink to={"/shipping"}>배송정보</NavLink>
+                <NavLink to={"/exchange"}>교환 및 반품</NavLink>
+                <NavLink to={"/terms"}>이용약관</NavLink>
+                <NavLink to={"/contact"}>문의하기</NavLink>
+                <NavLink to={"/privacy"}>개인정보처리방침</NavLink>
+                </li>
+            </ul>
             <p>&copy; {new Date().getFullYear()} Intern Shopping. All rights reserved.</p>
-        </footer>
+        </nav>
     );
 }
