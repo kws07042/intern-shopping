20d8850bbed9dfc011d4e1897c269789020f349e
geonyoungKO <kws07042@gmail.com>
2024-05-05 02:32:01 +0900

    Footer.jsx 수정 및 환경변수 적용

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
-        <footer className={'col-12'}>
-            <section>
-                <ul className={'flex'}>
-                    <li>배송정보</li>
-                    <li>교환 및 반품</li>
-                    <li>이용약관</li>
-                    <li>문의하기</li>
-                    <li>개인정보처리방침</li>
+        <footer>
+            <nav>
+                {/*React에서는 a 태그 대신에 Link사용*/}
+                <ul className="footer-nav">
+                    <li><Link to="/shipping">배송정보</Link></li>
+                    <li><Link to="/exchange">교환 및 반품</Link></li>
+                    <li><Link to="/terms">이용약관</Link></li>
+                    <li><Link to="/contact">문의하기</Link></li>
+                    <li><Link to="/privacy">개인정보처리방침</Link></li>
                 </ul>
-            </section>
+            </nav>
             <p>&copy; {new Date().getFullYear()} Intern Shopping. All rights reserved.</p>
         </footer>
     );
