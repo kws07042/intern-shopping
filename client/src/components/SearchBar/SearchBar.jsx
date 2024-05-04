a8a35783eaa8d280807dfecf68bc46a40d0c816d
JangHwanPark <wkd5878@gmail.com>
2024-05-04 17:08:36 +0900

    feat: 상품검색 클라이언트 처리(쿼리스트링 사용)

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 
 export default function SearchBar() {
+    const navigate = useNavigate();
     const [userInput, setUserInput] = React.useState('');
     const onChange = (e) => setUserInput(e.target.value);
 
+    // 상품정보 받아오기
+    const onClick = (e) => {
+        e.preventDefault();
+        console.log('검색어:', userInput);
+
+        // 입력값 인코딩 후 쿼리스트링 적용
+        const encodedInput = encodeURIComponent(userInput);
+        console.log('인코딩된 검색어:', encodedInput);
+        navigate(`products/search/product?name=${encodedInput}`);
+    }
 
     return (
         <form
-            action={process.env.REACT_APP_ENDPOINT_SEARCH}
-            method={'POST'}
+            //action={process.env.REACT_APP_ENDPOINT_SEARCH}
+            //method={'POST'}
         >
             <input
                 type="text"
@@ -16,7 +28,7 @@ export default function SearchBar() {
                 placeholder="상품을 검색하세요."
                 onChange={onChange}
             />
-            <button>검색</button>
+            <button onClick={onClick}>검색</button>
         </form>
     );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import SearchBar from "../components/SearchBar/SearchBar";
 import Products from "../components/Products/Products";
 import CarouselSlider from "../components/CarouselSlider/CarouselSlider";
+import {useSearchParams} from "react-router-dom";
 
 export default function Home() {
-    const url = '';
+    const param = useSearchParams();
+    console.log(`param: ${param}`);
+
     return (
         <div>
             <div><SearchBar/></div>
