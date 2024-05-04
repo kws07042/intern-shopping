ca0abd00d2a7d9a6a57cd824f515ffb2a0eb7ed3
JangHwanPark <wkd5878@gmail.com>
2024-05-04 10:38:38 +0900

    refactoring: 사용자 정보 출력 로직 이동 Home.jsx -> MyAccount.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,37 +1,9 @@
-import React, {useEffect, useState} from 'react';
-import {useAccount} from "../context/AuthContext";
-import {jwtDecode} from "jwt-decode";
+import React from 'react';
 
 export default function Home() {
-    const {...account} = useAccount();
-    console.log(`account: ${JSON.stringify(account)}`);
-
-    // 토큰 디코딩
-    let userEmail = '사용자 이메일 없음 (로그인 해주세요)';
-    let username = '사용자 이름 없음 (로그인 해주세요)';
-
-    if (account && account.token) {
-        const decoded = jwtDecode(account.token);
-        userEmail = `이메일 : ${decoded.email}`;
-        username = `사용자 이름: ${decoded.username}`;
-    }
-
     return (
         <div>
-            Home (아래는 기능 테스트용 로직)
-            <div>
-                {account && account.token ? '로그인됨' : '로그인 안됨'}
-            </div>
-            <div>
-                {account && account.token ? `테스트용 토큰 정보: ${account.token}` : '테스트용 토큰 정보 없음 (로그인 해주세요)'}
-            </div>
-            <div>{userEmail}</div>
-            <div>{username}</div>
-            <div>
-                {account && account.token && (
-                    <button onClick={account.logout}>로그아웃</button>
-                )}
-            </div>
+            Home
         </div>
     );
 }
--- client/src/pages/User/MyAccount.jsx
+++ client/src/pages/User/MyAccount.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import {useAccount} from "../../context/AuthContext";
+import {jwtDecode} from "jwt-decode";
 
 export default function MyAccount() {
+    const {...account} = useAccount();
+    console.log(`account: ${JSON.stringify(account)}`);
+
+    // 토큰 디코딩
+    let userEmail = '사용자 이메일 없음 (로그인 해주세요)';
+    let username = '사용자 이름 없음 (로그인 해주세요)';
+
+    if (account && account.token) {
+        const decoded = jwtDecode(account.token);
+        userEmail = `이메일 : ${decoded.email}`;
+        username = `사용자 이름: ${decoded.username}`;
+    }
+
     return (
         <div>
-            MyAccount
+            <div>
+                {account && account.token ? '로그인됨' : '로그인 안됨'}
+            </div>
+            <div>
+                {account && account.token ? `테스트용 토큰 정보: ${account.token}` : '테스트용 토큰 정보 없음 (로그인 해주세요)'}
+            </div>
+            <div>{userEmail}</div>
+            <div>{username}</div>
+            <div>
+                {account && account.token && (
+                    <button onClick={account.logout}>로그아웃</button>
+                )}
+            </div>
         </div>
     );
 }
