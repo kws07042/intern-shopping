5a7495f373f482ad6e3d6a28c83e8ee3986c0f8b
JangHwanPark <wkd5878@gmail.com>
2024-05-05 11:23:57 +0900

    feat: 장바구니 서버 개발

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -5,6 +5,15 @@ import Cart from "../models/cart.js";
 
 const router = express.Router();
 
+// 미들웨어
+router.use((req, res, next) => {
+    console.log('Cart middleware');
+    /*if (!req.session.cart) {
+        req.session.cart = new Cart();
+    }*/
+    next();
+})
+
 // GET: 장바구니 목록
 router.get('/', (req, res) => {
     const cart = new Cart();
@@ -22,6 +31,11 @@ router.get('/', (req, res) => {
             error: error.message
         });
     }
+});
+
+// POST: 장바구니 추가
+router.post('/', (req, res) => {
+    console.log(`req.body: ${JSON.stringify(req.body)}`);
 });
 
 export default router;
