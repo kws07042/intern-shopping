7455264765d6cc600ad2607a24c48ff2cf4ce42f
JangHwanPark <wkd5878@gmail.com>
2024-05-06 00:37:31 +0900

    feat: 장바구니 데이터 삭제 구현

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -36,10 +36,10 @@ router.get('/', async (req, res) => {
 // POST: 로그인한 사용자의 장바구니에 아이템 추가
 router.post('/add', async (req, res) => {
     console.log(`req.body: ${JSON.stringify(req.body)}`);
-    const { user_id, product_id, quantity } = req.body;
+    const { user_id, product_id } = req.body;
 
     // 유효성 검사
-    if (!user_id || !product_id || !quantity) {
+    if (!user_id || !product_id) {
         return res.status(400).json({
             message: 'Item id, name, and price are required'
         });
@@ -47,12 +47,39 @@ router.post('/add', async (req, res) => {
 
     const conn = await pool.getConnection();
     const sql = `INSERT INTO cart (user_id, product_id, quantity) VALUES (?, ?, ?)`;
-    const [result] = await conn.query(sql, [user_id, product_id, quantity]);
+    const [result] = await conn.query(sql, [user_id, product_id, 1]);
     console.log(`result: ${JSON.stringify(result)}`);
 
     res.status(200).json({
         message: 'Item added to cart',
         result
+    });
+});
+
+// POST: 로그인한 사용자의 장바구니에 아이템 삭제
+router.post('/remove', async (req, res) => {
+    console.log(`req.body: ${JSON.stringify(req.body)}`);
+    const { user_id, product_id } = req.body;
+
+    // 유효성 검사
+    if (!user_id || !product_id) {
+        return res.status(400).json({
+            message: 'Item id and user id are required'
+        });
+    }
+
+    const conn = await pool.getConnection();
+    const sql = `DELETE FROM cart WHERE user_id = ? AND product_id = ?`;
+    const [result] = await conn.query(sql, [user_id, product_id]);
+    console.log(`result: ${JSON.stringify(result)}`);
+
+    res.status(200).json({
+        message: 'Item removed from cart',
+        result
+    });
+
+    res.status(200).json({
+        message: 'Item removed from cart'
     });
 });
 
