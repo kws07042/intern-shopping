f18f6a0970be9181e55a13266d6a64397b134a7c
geonyoungKO <kws07042@gmail.com>
2024-05-14 12:02:47 +0900

    Navbar 와이어 프레임에 맞게 수정

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
-import {NavLink} from "react-router-dom";
-import {FaRegUser} from "react-icons/fa";
-import {BsCart3} from "react-icons/bs";
+import React, { useState } from 'react';
+import { NavLink } from "react-router-dom";
+import { FaRegUser } from "react-icons/fa";
+import { BsCart3 } from "react-icons/bs";
 
 export default function Navbar() {
+    // 드롭다운 메뉴 상태 관리를 위한 useState Hook 사용
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    // 드롭다운 메뉴 참
+    const openDropdown = () => setIsDropdownOpen(true);
+
+    // 드롭다운 메뉴 거짓
+    const closeDropdown = () => setIsDropdownOpen(false);
+
     return (
         <nav className={'s-navbar flex'}>
             <div>
                 <NavLink to={'/'}>Logo</NavLink>
             </div>
             <ul className={'s-nav-list flex'}>
-                <li>
+                <li onMouseEnter={openDropdown} onMouseLeave={closeDropdown}>
+                    {/* 마우스 이벤트 핸들러 추가 */}
                     <NavLink to={'/user/my-account'}>
                         <span><FaRegUser/></span>
-                        <span>마이페이지(Test)</span>
+                        <span>마이페이지</span>
                     </NavLink>
+                    {isDropdownOpen && (
+                        <ul className={'dropdown-menu'}>
+                            <li><NavLink to={'/user/test'}>test</NavLink></li>
+                            <li><NavLink to={'/user/test'}>test</NavLink></li>
+                            {/* 드랍다운 메뉴항목 */}
+                        </ul>
+                    )}
                 </li>
                 <li>
                     <NavLink to={'/user/shopping-cart'}>
@@ -32,3 +49,4 @@ export default function Navbar() {
         </nav>
     );
 }
+
